74a410f999e511919c35cf657aeb8ecbb145b8d5
nazzzzdza <nnnarrytta@gmail.com>
2026-05-04 16:34:49 +0200

    Create result.js

---
+++ commands/result.js
@@ -0,0 +1,44 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+const ALLOWED_CHANNEL_ID = "1446441803372036116";
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("results")
+    .setDescription("order results")
+    .addStringOption(option =>
+      option.setName("order id")
+        .setDescription("e.g. 001, 002")
+        .setRequired(true)
+                     
+    )
+    .addStringOption(option =>
+      option.setName("product")
+        .setDescription("product(s)")
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+
+    // 🔒 channel lock
+    if (interaction.channelId !== ALLOWED_CHANNEL_ID) {
+      return interaction.reply({
+        content: "this command can only be used in the waitlist channel.",
+        ephemeral: true
+      });
+    }
+
+    const orderNumber = interaction.options.getString("order id");
+    const product = interaction.options.getString("product");
+
+const message = `
+_ _   ıllı﹒﹒(mizuki emote) order (#${orderNumber}) ﹕done *!*
+_ _         ┆︎   ordered  :  (${product}) \`  🎀  \`
+_ _    ➔﹒﹒(mizuki emote) thank u for buying ♡
+`;
+
+    await interaction.reply({
+      content: message
+    });
+  }
+};
